d5f1caaa8dbd1eb8ea9efdc0e421fc73e28d7388
Muqueet Mohsen Chowdhury <mmc@berkeley.edu>
2026-04-16 16:53:53 +1000

    feature: setup Task Model in models.py

---
+++ task_tracker_cli/management/commands/task_cli.py
@@ -0,0 +1,3 @@
+from django.core.management.base import BaseCommand
+from task_tracker_cli.models import Task
+from datetime import datetime
--- task_tracker_cli/models.py
+++ task_tracker_cli/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    TODO = "todo"
+    IN_PROGRESS = "in_progress"
+    DONE = "done"
+
+    STATUS_CHOICES = [(TODO, "TO DO"), (IN_PROGRESS, "IN PROGRESS"), (DONE, "DONE")]
+
+    task_description = models.TextField()
+    task_status = models.CharField(max_length=15, choices=STATUS_CHOICES, default=TODO)
+    task_created_at = models.DateTimeField(auto_now_add=True)
+    task_updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.task_description
